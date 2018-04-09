378a7e434cfeb0537fc1afac7dcc2373d4f0ba9c
Rory Well <rory.e.well@gmail.com>
2018-04-09 02:13:24 -0500

    Successfully retrieve the username for each user's message

--- db/queries.js
+++ db/queries.js
@@ -5,16 +5,18 @@ const options = {
 };
 
 const pgp = require('pg-promise')(options);
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/narwhal_messages';
-const db = pgp(connectionString);
+const messagesConnectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/narwhal_messages';
+const usersConnectionString = process.env.USERS_DATABASE_URL || 'postgres://localhost:5432/narwhal_users';
+const dbMessages = pgp(messagesConnectionString);
+const dbUsers = pgp(usersConnectionString);
 
 const messages = {
   // Add a message to a topic
   createMessage: async(userId, topicId, messageText) => {
     try {
-      return await db.one('INSERT INTO topic_message(message_text, topic_id, author_id) ' +
+      return await dbMessages.one('INSERT INTO topic_message(message_text, topic_id, author_id) ' +
           'VALUES(${messageText}, ${topicId}, ${userId}) ' +
-          'RETURNING id, message_text', 
+          'RETURNING id, message_text, topic_id, author_id, create_date, last_update_date', 
         {
           messageText: messageText,
           topicId: topicId,
@@ -28,7 +30,23 @@ const messages = {
   // Return the last 200 messages for a topic
   getMessageHistoryForTopic: async(topicId) => {
     try {
-      return await db.any('SELECT * FROM topic_message WHERE topic_id = ${topicId} ORDER BY create_date LIMIT 200',  { topicId: topicId });
+      const messageResults = await dbMessages.any('SELECT id, message_text, topic_id, author_id, create_date, last_update_date ' +
+          'FROM topic_message WHERE topic_id = ${topicId} ORDER BY create_date LIMIT 200',  { topicId: topicId }
+      );
+      const userIds = messageResults.map((message) => {
+        return [message.author_id];
+      });
+      const users = await dbUsers.any('SELECT id, username FROM users WHERE id IN ($1)', [1, userIds]);
+      const messages = [];
+      for (let message of messageResults) {
+        for (let user of users) {
+          if (message.author_id = user.id) {
+            message.username = user.username;
+            messages.push(message);
+          }
+        }
+      }
+      return messages;
     } catch (e) {
       console.log(e);
     }
